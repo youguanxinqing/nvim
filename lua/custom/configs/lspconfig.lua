23fefe3ee480d03a855b59a418a43777eee2b886
youguanxinqing <youguanxinqing@qq.com>
2024-02-28 23:45:11 +0800

    feat: pyright add root_dir

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,84 +1,88 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require("lspconfig")
+local lspconfig = require "lspconfig"
 
 -- fix: attempt to index field 'semanticTokensProvider' (a nil value)
 -- refs: https://github.com/neovim/nvim-lspconfig/issues/2542#issuecomment-1547019213
 local on_init = function(client, initialization_result)
-	local _ = initialization_result
-	if client.server_capabilities then
-		client.server_capabilities.documentFormattingProvider = false
-		client.server_capabilities.semanticTokensProvider = false -- turn off semantic tokens
-	end
+  local _ = initialization_result
+  if client.server_capabilities then
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.semanticTokensProvider = false -- turn off semantic tokens
+  end
 end
 
-lspconfig.lua_ls.setup({
-	on_init = on_init,
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "lua" },
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-				disbale = {
-					"missing-fields",
-				},
-				severity = {
-					missingFields = "Error!",
-				},
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-					[vim.fn.stdpath("data") .. "/lazy/extensions/nvchad_types"] = true,
-					[vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
-				},
-				maxPreload = 100000,
-				preloadFileSize = 10000,
-			},
-		},
-	},
-})
+lspconfig.lua_ls.setup {
+  on_init = on_init,
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "lua" },
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { "vim" },
+        disbale = {
+          "missing-fields",
+        },
+        severity = {
+          missingFields = "Error!",
+        },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/extensions/nvchad_types"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+}
 
-lspconfig.rust_analyzer.setup({
-	on_init = on_init,
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "rust" },
-	root = lspconfig.util.root_pattern("Cargo.toml"),
-})
+lspconfig.rust_analyzer.setup {
+  on_init = on_init,
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "rust" },
+  root = lspconfig.util.root_pattern "Cargo.toml",
+}
 
-lspconfig.gopls.setup({
-	on_init = on_init,
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "go", "gomod", "gowork", "gotmpl" },
-	root = lspconfig.util.root_pattern("go.mod"),
-	-- cmd_env = { GOFLAGS = "-tags=tag1,tag2" },
-})
+lspconfig.gopls.setup {
+  on_init = on_init,
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root = lspconfig.util.root_pattern "go.mod",
+  -- cmd_env = { GOFLAGS = "-tags=tag1,tag2" },
+}
 
-lspconfig.pyright.setup({
-	on_init = on_init,
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "python" },
-	settings = {
-		python = {
-			analysis = {
-				diagnosticMode = "openFilesOnly",
-				typeCheckingMode = "basic",
-				diagnosticSeverityOverrides = {
-					reportGeneralTypeIssues = "none",
-				},
-			},
-		},
-	},
-})
+lspconfig.pyright.setup {
+  on_init = on_init,
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = function(fname)
+    local default_config = require("lspconfig.server_configurations.pyright").default_config
+    return default_config.root_dir(fname) or require("lspconfig.util").find_git_ancestor(fname)
+  end,
+  filetypes = { "python" },
+  settings = {
+    python = {
+      analysis = {
+        diagnosticMode = "openFilesOnly",
+        typeCheckingMode = "basic",
+        diagnosticSeverityOverrides = {
+          reportGeneralTypeIssues = "none",
+        },
+      },
+    },
+  },
+}
