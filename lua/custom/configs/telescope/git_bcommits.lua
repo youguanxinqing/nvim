d3e0ff43871297fdbb15fffc2ae4721f4afdf6df
youguanxinqing <youguanxinqing@qq.com>
2024-03-29 22:08:17 +0800

    format code

--- lua/custom/configs/telescope/git_bcommits.lua
+++ lua/custom/configs/telescope/git_bcommits.lua
@@ -1,14 +1,14 @@
-local is_present, telescope = pcall(require, "telescope")
+local is_present, _ = pcall(require, "telescope")
 if not is_present then
-	return
+  return
 end
 
 local M = {}
 
 local git_bcommits_bak = require("telescope.builtin").git_bcommits
 M.m_git_bcommits = function(opts)
-	opts.git_command = { "git", "log", "--pretty=%C(auto)%h {%as %cn} %s", "--abbrev-commit", "--follow" }
-	git_bcommits_bak(opts)
+  opts.git_command = { "git", "log", "--pretty=%C(auto)%h {%as %cn} %s", "--abbrev-commit", "--follow" }
+  git_bcommits_bak(opts)
 end
 
 -- overrides
