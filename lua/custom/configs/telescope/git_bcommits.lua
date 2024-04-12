137c226f403f788471c366c10caa24688fbc4e6b
youguanxinqing <youguanxinqing@qq.com>
2024-04-12 23:23:09 +0800

    override telescope git_commit

--- lua/custom/configs/telescope/git_bcommits.lua
+++ lua/custom/configs/telescope/git_bcommits.lua
@@ -11,5 +11,12 @@ M.m_git_bcommits = function(opts)
   git_bcommits_bak(opts)
 end
 
+local git_commits_bak = require("telescope.builtin").git_commits
+M.m_git_commits = function(opts)
+  opts.git_command = { "git", "log", "--pretty=%C(auto)%h {%as %cn} %s", "--abbrev-commit", "--", "." }
+  git_commits_bak(opts)
+end
+
 -- overrides
 require("telescope.builtin").git_bcommits = M.m_git_bcommits
+require("telescope.builtin").git_commits = M.m_git_commits
