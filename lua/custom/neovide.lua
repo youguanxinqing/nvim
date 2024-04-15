97ab67cb79800d957edc4863b54ab1f51fa6b7c3
youguanxinqing <youguanxinqing@qq.com>
2024-04-15 20:08:22 +0800

    fix: failed to display on windows wsl

--- lua/custom/neovide.lua
+++ lua/custom/neovide.lua
@@ -1,6 +1,8 @@
 vim.g.neovide_scale_factor = 1.1
 vim.g.neovide_transparency = 0.96
 vim.g.neovide_window_blurred = true
+-- FIX: failed to display on windows wsl
+vim.o.guifont = "JetBrainsMono Nerd Font"
 
 vim.keymap.set("n", "<S-Insert>", '"+p')
 vim.keymap.set("i", "<S-Insert>", "<C-R>+")
