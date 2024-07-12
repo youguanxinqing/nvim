f722a114ab9006d254ae6c84b1711d259298c26b
youguanxinqing <youguanxinqing@qq.com>
2024-07-12 23:04:18 +0800

    replace vimscript by lua

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -26,13 +26,6 @@
 -- https://snippet-generator.app/
 vim.g.vscode_snippets_path = "./lua/custom/snippets"
 
-vim.cmd [[
-autocmd FileType go command! GoGenEnumToString lua require("custom.configs.gen-go-code").enum_to_string()
-autocmd FileType go command! GoRunUnitTest lua require("custom.configs.run-anything").run_unit_test_for_go()
-
-autocmd FileType python command! PyRun lua require("custom.configs.run-anything").run_current_script_for_py()
-]]
-
 -- vim.cmd command!
 vim.api.nvim_create_user_command(
   "UncolorAllWords",
@@ -48,6 +41,33 @@ vim.api.nvim_create_user_command("GitHistory", "Telescope git_bcommits", { bang 
 vim.api.nvim_create_user_command("GitBlame", "Gitsigns blame", { bang = true })
 vim.api.nvim_create_user_command("GitToggleLineBlame", "Gitsigns toggle_current_line_blame", { bang = true })
 vim.api.nvim_create_user_command("GitDiffThis", "Gitsigns diffthis", { bang = true })
+
+-- vim.cmd autocmd for command!
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python" },
+  callback = function()
+    vim.api.nvim_create_user_command(
+      "PyRun",
+      'lua require("custom.configs.run-anything").run_current_script_for_py()',
+      { bang = true }
+    )
+  end,
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "go" },
+  callback = function()
+    vim.api.nvim_create_user_command(
+      "GoGenEnumToString",
+      'lua require("custom.configs.gen-go-code").enum_to_string()',
+      { bang = true }
+    )
+    vim.api.nvim_create_user_command(
+      "GoRunUnitTest",
+      'lua require("custom.configs.run-anything").run_unit_test_for_go()',
+      { bang = true }
+    )
+  end,
+})
 
 if vim.g.neovide then
   require "custom.neovide"
