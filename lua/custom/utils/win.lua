48bf624f2406af56506956a7a82b72c4a9c9e578
youguanxinqing <youguanxinqing@qq.com>
2024-07-27 02:03:04 +0800

    add M.display_content_to_window

---
+++ lua/custom/utils/win.lua
@@ -0,0 +1,42 @@
+local tbl_utils = require "lua.custom.utils.table"
+local buf_utils = require "lua.custom.utils.buf"
+
+local M = {}
+
+function M.display_content_to_window(window_name, title, content)
+  local _real_name = string.format("%s/%s", vim.loop.cwd(), window_name)
+
+  local listed_bufs = buf_utils.listed_buf_infos()
+
+  local buf_id, win_id = nil, nil
+  -- find existed window
+  for _, buf_info in ipairs(listed_bufs) do
+    if _real_name == buf_info.name then
+      buf_id = buf_info.bufnr
+      if vim.fn.len(buf_info.windows) ~= 0 then
+        win_id = buf_info.windows[1]
+      end
+    end
+  end
+
+  -- if it does not open, create a new window
+  if win_id == nil then
+    win_id = vim.api.nvim_open_win(0, false, {
+      split = "right",
+    })
+  end
+
+  -- if no buf, create a new buffer
+  if buf_id == nil then
+    buf_id = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_buf_set_name(buf_id, window_name)
+  end
+
+  vim.api.nvim_win_set_buf(win_id, buf_id)
+
+  local lines = { title, "---" }
+  tbl_utils.extend(lines, vim.fn.split(content, "\n"))
+  vim.api.nvim_buf_set_lines(buf_id, 0, vim.fn.len(lines), false, lines)
+end
+
+return M
