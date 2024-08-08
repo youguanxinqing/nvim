dc4b56919bb5c26e281fd4b174e550302da6e9a6
youguanxinqing <youguanxinqing@qq.com>
2024-08-08 22:12:01 +0800

    support create action for find-files

--- lua/custom/configs/telescope/search.lua
+++ lua/custom/configs/telescope/search.lua
@@ -3,6 +3,7 @@ local finders = require "telescope.finders"
 local make_entry = require "telescope.make_entry"
 local conf = require("telescope.config").values
 local sorters = require "telescope.sorters"
+local global_state = require "telescope.state"
 
 local buf_utils = require "custom.utils.buf"
 local string_uitils = require "custom.utils.string"
@@ -100,13 +101,42 @@ function M.search_in_current_buffer(opts)
     :find()
 end
 
+local FIND_FILES_PROMPT_KEY = "find-files-prompt"
+
+local function create_file_or_dir()
+  local path = global_state.get_global_key(FIND_FILES_PROMPT_KEY)
+  if vim.fn.exists(path) == 1 then
+    return
+  end
+
+  local y_or_n = vim.fn.input(string.format("'%s' is not existed, would you create it? y/N: ", path))
+  y_or_n = string.lower(vim.fn.trim(y_or_n))
+  if y_or_n ~= "y" then
+    return
+  end
+
+  -- create directory if endswith '/' character
+  -- otherwise create file
+  if string.sub(path, -1) == "/" then
+    vim.system { "mkdir", "-p", path }
+  else
+    vim.system { "touch", path }
+  end
+
+  print "create ok!"
+end
+
 function M.find_files_from_here(opts)
   opts = opts or {}
 
   local files_from_here = buf_utils.get_cur_buf_dir()
+  if string.sub(files_from_here, 1, 1) ~= "." then
+    files_from_here = "./" .. files_from_here
+  end
 
   local live_grepper = finders.new_job(function(prompt)
     prompt = vim.fn.trim(prompt)
+    global_state.set_global_key(FIND_FILES_PROMPT_KEY, prompt)
 
     local dir = nil
     if string.match(prompt, "%s+") ~= nil then
@@ -128,6 +158,10 @@ function M.find_files_from_here(opts)
       dir = table_uitils.join(chunks, "/")
     end
 
+    if string.sub(dir, 1, 1) ~= "." then
+      dir = "./" .. dir
+    end
+
     local notice = string.format("dir:%s,prompt:%s", dir, prompt)
     print(notice)
     return flatten { { "rg", "--files", "--color", "never" }, "--", prompt, dir }
@@ -141,6 +175,10 @@ function M.find_files_from_here(opts)
       previewer = conf.grep_previewer(opts),
       sorter = conf.file_sorter(opts),
       default_text = files_from_here,
+      attach_mappings = function(_, keymaps)
+        keymaps({ "i", "n" }, "<c-a>", create_file_or_dir)
+        return true
+      end,
     })
     :find()
 end
