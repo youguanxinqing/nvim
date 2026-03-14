f0eb8c9e4dccefaf0c6989715778db7cbcb4e6d2
youguanxinqing <youguanxinqing@qq.com>
2026-03-14 15:08:37 +0800

    feat(plugins): add snacks.nvim plugin with custom options

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -366,6 +366,25 @@ local plugins = {
     end,
   },
   {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = false },
+      dashboard = { enabled = false },
+      explorer = { enabled = false },
+      indent = { enabled = false },
+      input = { enabled = true },
+      picker = { enabled = true },
+      notifier = { enabled = false },
+      quickfile = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false },
+      words = { enabled = false },
+    },
+  },
+  {
     "folke/sidekick.nvim",
     cmd = { "Sidekick" },
     dependencies = {
